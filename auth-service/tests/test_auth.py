c597aa285ad55d9881226aa3c7bcf1184df538b2
leviator30 <leviator_30@yahoo.com>
2026-05-08 20:32:33 +0300

    small modifications 1

--- auth-service/tests/test_auth.py
+++ auth-service/tests/test_auth.py
@@ -4,7 +4,7 @@ from auth_service import app
 import os
 from pymongo import MongoClient
 
-# Folosim un client sincron (pymongo) pentru a curăța baza de date între teste
+# Deletes all existing users, providing a "clean slate".
 @pytest.fixture(scope="module", autouse=True)
 def clean_db():
     mongo_uri = os.getenv("MONGO_DETAILS", "mongodb://localhost:27017")
@@ -12,23 +12,27 @@ def clean_db():
     db_client.users_db.users.delete_many({})
     db_client.close()
 
+# Creates a TestClient.
 @pytest.fixture
 def client():
     with TestClient(app) as c:
         yield c
 
+# The system should successfully save the user and return a success message.
 def test_register_user(client):
     payload = {"username": "testuser", "password": "securepassword"}
     response = client.post("/register", json=payload)
-    assert response.status_code == 201 or response.status_code == 200 # depinde ce ai pus in cod
+    assert response.status_code == 201
     assert "message" in response.json()
 
+# The system should validate the user and return an 'access_token'.
 def test_login_success(client):
     payload = {"username": "testuser", "password": "securepassword"}
     response = client.post("/login", json=payload)
     assert response.status_code == 200
     assert "access_token" in response.json()
 
+# The system should reject the attempt and deny access.
 def test_login_wrong_password(client):
     payload = {"username": "testuser", "password": "wrongpassword"}
     response = client.post("/login", json=payload)
